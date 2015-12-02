ec4e31ff9f9c811598162570d5c3cea03ec9e1c1
Remco Bouckaert <higgs.ml@gmail.com>
2015-12-02 14:46:31 +1300

    handle loss of focus in location editor

--- src/beast/app/beauti/SLocationInputEditor.java
+++ src/beast/app/beauti/SLocationInputEditor.java
@@ -207,6 +207,7 @@ public class SLocationInputEditor extends ListInputEditor {
             }
 
         });
+        table.putClientProperty("terminateEditOnFocusLost", Boolean.TRUE);
         table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
         table.setRowHeight(24);
         table.getColumnModel().getColumn(0).setPreferredWidth(300);
